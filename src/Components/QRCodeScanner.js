8696ae615b84479fb3fa434e386a4d80f060e8cd
Shady-Omar <Shady-Omar>
2023-11-07 01:26:45 +0200

    modi

--- src/Components/QRCodeScanner.js
+++ src/Components/QRCodeScanner.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 import {QrReader} from "react-qr-reader";
 
 const QRCodeScanner = () => {
-  const [selected, setSelected] = useState("environment");
+  // const [selected, setSelected] = useState("environment");
   const [startScan, setStartScan] = useState(false);
   const [loadingScan, setLoadingScan] = useState(false);
   const [data, setData] = useState("");
@@ -25,10 +25,10 @@ const QRCodeScanner = () => {
   return (
     <div>
       <h1>Hello CodeSandbox</h1>
-      <h2>
+      {/* <h2>
         Last Scan:
         {selected}
-      </h2>
+      </h2> */}
 
       <button
         onClick={() => {
@@ -39,12 +39,15 @@ const QRCodeScanner = () => {
       </button>
       {startScan && (
         <>
-          <select onChange={(e) => setSelected(e.target.value)}>
+          {/* <select onChange={(e) => setSelected(e.target.value)}>
             <option value={"environment"}>Back Camera</option>
             <option value={"user"}>Front Camera</option>
-          </select>
+          </select> */}
           <QrReader
-            facingMode={selected}
+            constraints={{
+              audio: true,
+              video: { facingMode: "environment" }
+            }}
             delay={1000}
             onError={handleError}
             onScan={handleScan}
