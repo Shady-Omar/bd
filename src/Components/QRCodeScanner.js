8a5d89488835f7f003650197841c3fa74a122d86
Shady-Omar <Shady-Omar>
2023-11-07 01:15:33 +0200

    modd

--- src/Components/QRCodeScanner.js
+++ src/Components/QRCodeScanner.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { QrReader } from "react-qr-reader";
 
 const QRCodeScanner = () => {
-  // const [selected, setSelected] = useState("environment");
+  const [selected, setSelected] = useState("environment");
   const [startScan, setStartScan] = useState(false);
   const [loadingScan, setLoadingScan] = useState(false);
   const [data, setData] = useState("");
@@ -18,15 +18,16 @@ const QRCodeScanner = () => {
       // setPrecScan(scanData);
     }
   };
-
   const handleError = (err) => {
     console.error(err);
   };
-
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      {/* <h2>Last Scan: {selected}</h2> */}
+      <h2>
+        Last Scan:
+        {selected}
+      </h2>
 
       <button
         onClick={() => {
@@ -37,15 +38,16 @@ const QRCodeScanner = () => {
       </button>
       {startScan && (
         <>
-          {/* <select onChange={(e) => setSelected(e.target.value)}>
+          <select onChange={(e) => setSelected(e.target.value)}>
             <option value={"environment"}>Back Camera</option>
             <option value={"user"}>Front Camera</option>
-          </select> */}
+          </select>
           <QrReader
-            facingMode="environment" // Set facingMode to "environment" for the back camera
+            facingMode={selected}
             delay={1000}
             onError={handleError}
             onScan={handleScan}
+            // chooseDeviceId={()=>selected}
             style={{ width: "300px" }}
           />
         </>
