ab6bb6a737b32c84434ec0f1a4f5dffb963eb7de
Shady-Omar <Shady-Omar>
2023-11-07 01:33:41 +0200

    mod

--- src/Components/QRCodeScanner.js
+++ src/Components/QRCodeScanner.js
@@ -1,34 +1,32 @@
 import "../styles.css";
 import { useState } from "react";
-import {QrReader} from "react-qr-reader";
+import { QrReader } from "react-qr-reader";
 
 const QRCodeScanner = () => {
-  // const [selected, setSelected] = useState("environment");
   const [startScan, setStartScan] = useState(false);
   const [loadingScan, setLoadingScan] = useState(false);
   const [data, setData] = useState("");
+  const [scannedLink, setScannedLink] = useState(""); // New state variable for scanned link
 
   const handleScan = async (scanData) => {
     setLoadingScan(true);
-    console.log(`loaded data data`, scanData);
+    console.log("Scanned data:", scanData);
     if (scanData && scanData !== "") {
-      console.log(`loaded >>>`, scanData);
+      console.log("Scanned:", scanData);
       setData(scanData);
+      setScannedLink(scanData); // Update scanned link state
       setStartScan(false);
       setLoadingScan(false);
-      // setPrecScan(scanData);
     }
   };
+
   const handleError = (err) => {
     console.error(err);
   };
+
   return (
     <div>
       <h1>Hello CodeSandbox</h1>
-      {/* <h2>
-        Last Scan:
-        {selected}
-      </h2> */}
 
       <button
         onClick={() => {
@@ -39,24 +37,22 @@ const QRCodeScanner = () => {
       </button>
       {startScan && (
         <>
-          {/* <select onChange={(e) => setSelected(e.target.value)}>
-            <option value={"environment"}>Back Camera</option>
-            <option value={"user"}>Front Camera</option>
-          </select> */}
           <QrReader
-            facingMode={'environment'}
             delay={1000}
             onError={handleError}
             onScan={handleScan}
-            // chooseDeviceId={()=>selected}
             style={{ width: "300px" }}
-            onResult={ handleScan }
-            constraints={ {facingMode: 'environment'} }
+            constraints={{ facingMode: "environment" }}
           />
         </>
       )}
       {loadingScan && <p>Loading</p>}
-      {data !== "" && <p>{data}</p>}
+      {data !== "" && (
+        <div>
+          <p>{data}</p>
+          <p>Scanned Link: {scannedLink}</p> {/* Display scanned link */}
+        </div>
+      )}
     </div>
   );
 };
