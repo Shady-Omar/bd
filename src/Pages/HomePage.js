4f0b6f7e73500b6b68d3d350ec1a63d69f3fe788
Shady-Omar <Shady-Omar>
2023-11-11 18:11:03 +0200

    mod

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import Logo from '../assets/logo.svg'
-import QRCodeScanner from '../Components/QRCodeScanner';
+import lens from '../assets/lens.svg'
+// import QRCodeScanner from '../Components/QRCodeScanner';
+import { QrReader } from "react-qr-reader";
 import { db } from '../firebase';
 import { collection, getDocs, query, where } from 'firebase/firestore';
 import CompanyInfoPopup from "../Components/BoycottPopup";
@@ -14,6 +16,13 @@ function HomePage() {
   const [selectedCompany, setSelectedCompany] = useState(null);
 
   const [companies, setCompanies] = useState([]);
+
+  // For QR CODE SCAN:
+  const [startScan, setStartScan] = useState(false);
+  const [loadingScan, setLoadingScan] = useState(false);
+  const [data, setData] = useState('No result');
+  const [boycottText, setboycottText] = useState("");
+  // *****
 
   useEffect(() => {
     const fetchCompanies = async () => {
@@ -61,18 +70,59 @@ function HomePage() {
     setSelectedCompany(null);
   };
 
+  // For QR CODE SCAN:
+
+  const handleScan = async (scanData) => {
+    setLoadingScan(true);
+
+    if (scanData && scanData !== '') {
+      // Query Firestore to check if the received URL exists in the 'companies' collection
+      const companiesCollection = collection(db, 'companies');
+      const q = query(companiesCollection, where('url', '==', scanData));
+      const querySnapshot = await getDocs(q);
+
+      if (!querySnapshot.empty) {
+        querySnapshot.forEach((doc) => {
+          const data = doc.data();
+          const boycott = data.boycott; // Assuming "boycott" is a field in your Firestore document
+
+          if (boycott === true) {
+            // If "boycott" field equals true, log a message
+            setboycottText(<p className="text-center font-semibold text-lg mb-0">This brand supports the Israeli occupation<br/>هذه الشركة تدعم الاحتلال الإسرائيلي</p>)
+          } else {
+            // "boycott" is not true
+            setboycottText(<p className="text-center font-semibold text-lg mb-0">This brand supports the Palestine<br/>هذه الشركة تدعم فلسطين </p>)
+          }
+        });
+      } else {
+        // URL does not exist in Firestore
+        console.log('URL not found in Firestore:', scanData);
+        setboycottText(<p className="text-center font-semibold text-md mb-0">Not recognised</p>)
+      }
+
+      setData(scanData);
+      setStartScan(false);
+      setLoadingScan(false);
+    }
+  };
+
+  const handleError = (err) => {
+    console.error(err);
+  };
+  // *****
+
   return (
     <div className="flex justify-center w-full text-[#212529]">
       <div className="container px-3">
         <div className="my-12 flex justify-center items-center">
-          <img className="w-[250px]" src={Logo} alt=""/>
+          <img className="w-[200px]" src={Logo} alt=""/>
         </div>
 
         <div className="mt-0 mb-12 flex justify-center items-center">
-          <p className="text-center mb-0 text-[2em] about-text">A platform promoting conscientious consumerism by identifying/facilitating access to information about which companies do or do not support the illegal Israeli Occupation of Palestine.<br />منصة تدعم الاستهلاك الواعي من خلال تسهيل معرفة الشركات التي تدعم وتلك التي لا تدعم الاحتلال الإسرائيلي الغاشم لفلسطين</p>
+          <p className="text-center mb-0 lg:text-[2em] about-text">A platform promoting conscientious consumerism by identifying/facilitating access to information about which companies do or do not support the illegal Israeli Occupation of Palestine.<br />منصة تدعم الاستهلاك الواعي من خلال تسهيل معرفة الشركات التي تدعم وتلك التي لا تدعم الاحتلال الإسرائيلي الغاشم لفلسطين</p>
         </div>
 
-        <form className="relative" method="post" id="searchForm" width="100%">
+        <form className="relative" id="searchForm" width="100%">
           <div className="bar">
             <input
               type="text"
@@ -83,6 +133,16 @@ function HomePage() {
               value={search}
               onChange={(e) => handleSearch(e.target.value)}
             />
+            <button
+            type="button"
+              className="lens-div"
+              onClick={() => {
+                setStartScan(!startScan);
+                setData('No result');
+              }}
+            >
+              <img className="lens" id="searchByCamera" src={lens} alt="" title="Search by Image"/>
+            </button>
           </div>
             {suggestions.length > 0 && (
               <ul className="mt-2 border rounded shadow-lg absolute z-10 bg-white w-full">
@@ -106,8 +166,53 @@ function HomePage() {
         )}
 
         {/* QR Code Scanner: */}
-        <QRCodeScanner />
-
+        {/* <QRCodeScanner /> */}
+        
+        <div>
+          {/* <button
+            className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 my-5"
+            onClick={() => {
+              setStartScan(!startScan);
+              setData('No result');
+            }}
+          >
+            {startScan ? "Stop Scan" : "Start Scan"}
+          </button> */}
+          {startScan && (
+            <>
+              <QrReader
+                delay={1000}
+                onError={handleError}
+                onScan={handleScan}
+                style={{ width: "300px" }}
+                onResult={(result, error) => {
+                  if (!!result) {
+                    setData(result?.text);
+                    setStartScan(!startScan);
+                  }
+        
+                  if (!!error) {
+                    console.info(error);
+                  }
+                }}
+                constraints={{ facingMode: "environment" }}
+              />
+            </>
+          )}
+          {data !== "" && (
+            <div className="mt-5">
+              {data !== "No result"
+                ? <a className=" text-green-600" href={data}>{data}</a>
+                :
+                  <>
+                    <p>{data}</p>
+                    {boycottText}
+                  </>
+              }
+            </div>
+          )}
+          {loadingScan && <p>Loading</p>}
+        </div>
 
         <Investigations/>
       </div>
